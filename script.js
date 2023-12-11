567b5494b401842dcd368b5f48779f0f126f2a13
Kirk <Wuzhuocheng2020@hotmail.com>
2023-12-11 11:14:39 +0800

    Search function completes

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-// Add your JavaScript code here if needed
-// For example: interactive behavior, form validation, etc.
+function search(event) {
+    event.preventDefault(); // Prevent form submission
+  
+    // Get the search query from the input field
+    const searchInput = document.getElementById('search-input');
+    const query = searchInput.value.trim().toLowerCase();
+  
+    // Perform the search
+    const products = document.querySelectorAll('.product');
+    for (const product of products) {
+      const productTitle = product.querySelector('h3').textContent.toLowerCase();
+      const productDescription = product.querySelector('p').textContent.toLowerCase();
+  
+      if (productTitle.includes(query) || productDescription.includes(query)) {
+        product.style.display = 'block'; // Show matching products
+      } else {
+        product.style.display = 'none'; // Hide non-matching products
+      }
+    }
+  }
